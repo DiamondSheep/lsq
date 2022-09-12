d93d64fbbf39d95db8c7adab22c76de8acc46b89
gaoyangcheng <gaoyangcheng576@gmail.com>
2022-09-12 20:35:04 +0800

    add detection evaluation function(without fix)

--- eval_utils.py
+++ eval_utils.py
@@ -1,87 +1,101 @@
+import json
+import os
+import sys
+import numpy as np
 import torch 
 import torch.nn as nn
+from pathlib import Path
+from threading import Thread
+from tqdm import tqdm
 from progress.bar import Bar
 
+# YOLO modules
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # root directory
+YOLOROOT = os.path.join(ROOT, 'yolov3')
+if str(YOLOROOT) not in sys.path:
+    sys.path.append(str(YOLOROOT))  # add YOLOROOT to PATH
+YOLOROOT = Path(os.path.relpath(YOLOROOT, Path.cwd()))  # relative
+
+from yolov3.models.common import DetectMultiBackend
+from yolov3.utils.callbacks import Callbacks
+from yolov3.utils.datasets import create_dataloader
+from yolov3.utils.general import (LOGGER, NCOLS, box_iou, check_dataset, check_img_size, check_requirements, check_yaml,
+                           coco80_to_coco91_class, colorstr, increment_path, non_max_suppression, print_args,
+                           scale_coords, xywh2xyxy, xyxy2xywh)
+from yolov3.utils.metrics import ConfusionMatrix, ap_per_class
+from yolov3.utils.plots import output_to_target, plot_images, plot_val_study
+from yolov3.utils.torch_utils import select_device, time_sync
+
+'''
+CLASSIFICATION
+'''
 @torch.no_grad()
 def validate(model, testloader, logger, configs, verbose=True):
-    
-    if configs.task == 'classification':
-        total, correct = 0, 0
-        bar = Bar('Testing', max=len(testloader))
-        model.eval()
-        top1_error = AverageMeter()
-        top5_error = AverageMeter()
-        with torch.no_grad():
-            for batch_idx, (inputs, targets) in enumerate(testloader):
-                inputs, targets = inputs.cuda(), targets.cuda()
-                outputs = model(inputs)
-                loss = torch.ones(1)
-                single_top1_error, loss, single_top5_error = compute_singlecrop(
-                    outputs=outputs, labels=targets, loss=loss, 
-                    top5_flag=True, mean_flag=True)
-                top1_error.update(single_top1_error)
-                top5_error.update(single_top5_error)
-                #_, predicted = outputs.max(1)
-                #total += targets.size(0)
-                #correct += predicted.eq(targets).sum().item()
-                #acc = correct / total
+    total, correct = 0, 0
+    bar = Bar('Testing', max=len(testloader))
+    model.eval()
+    top1_error = AverageMeter()
+    top5_error = AverageMeter()
+    for batch_idx, (inputs, targets) in enumerate(testloader):
+        inputs, targets = inputs.cuda(), targets.cuda()
+        outputs = model(inputs)
+        loss = torch.ones(1)
+        single_top1_error, loss, single_top5_error = compute_singlecrop(
+            outputs=outputs, labels=targets, loss=loss, 
+            top5_flag=True, mean_flag=True)
+        top1_error.update(single_top1_error)
+        top5_error.update(single_top5_error)
+        #_, predicted = outputs.max(1)
+        #total += targets.size(0)
+        #correct += predicted.eq(targets).sum().item()
+        #acc = correct / total
 
-                bar.suffix = f'({batch_idx + 1}/{len(testloader)}) | ETA: {bar.eta_td} | top1: {100.0 - top1_error.avg} | top5: {100.0 - top5_error.avg}'
-                bar.next()
-        if verbose:
-            logger.info('\nFinal result: top1 %.3f%% , top5 %.3f%%' % (100.0 - top1_error.avg, 100.0 - top5_error.avg))
-        bar.finish()
-        model.train()
-        return 100.0 - top1_error.avg, 100.0 - top5_error.avg
+        bar.suffix = f'({batch_idx + 1}/{len(testloader)}) | ETA: {bar.eta_td} | top1: {100.0 - top1_error.avg} | top5: {100.0 - top5_error.avg}'
+        bar.next()
+    if verbose:
+        logger.info('\nFinal result: top1 %.3f%% , top5 %.3f%%' % (100.0 - top1_error.avg, 100.0 - top5_error.avg))
+    bar.finish()
+    model.train()
+    return 100.0 - top1_error.avg, 100.0 - top5_error.avg
 
-    elif configs.task == 'detection':
-        model.eval()
-        with torch.no_grad():
-            for batch_idx, data in enumerate(testloader):
-                scale = data['scale']
-                inputs, targets = data['img'].permute(
-                0, 3, 1, 2).cuda(), data['annot'].cuda()
-                outputs = model(inputs)
-                # TODO
-                c = input()
-
+@torch.no_grad()
 def compute_singlecrop(outputs, labels, loss, top5_flag=False, mean_flag=False):
-    with torch.no_grad():
-        if isinstance(outputs, list):
-            top1_loss = []
-            top1_error = []
-            top5_error = []
-            for i in range(len(outputs)):
-                top1_accuracy, top5_accuracy = accuracy(outputs[i], labels, topk=(1, 5))
-                top1_error.append(100 - top1_accuracy)
-                top5_error.append(100 - top5_accuracy)
-                top1_loss.append(loss[i].item())
-        else:
-            top1_accuracy, top5_accuracy = accuracy(outputs, labels, topk=(1,5))
-            top1_error = 100 - top1_accuracy
-            top5_error = 100 - top5_accuracy
-            top1_loss = loss.item()
+    if isinstance(outputs, list):
+        top1_loss = []
+        top1_error = []
+        top5_error = []
+        for i in range(len(outputs)):
+            top1_accuracy, top5_accuracy = accuracy(outputs[i], labels, topk=(1, 5))
+            top1_error.append(100 - top1_accuracy)
+            top5_error.append(100 - top5_accuracy)
+            top1_loss.append(loss[i].item())
+    else:
+        top1_accuracy, top5_accuracy = accuracy(outputs, labels, topk=(1,5))
+        top1_error = 100 - top1_accuracy
+        top5_error = 100 - top5_accuracy
+        top1_loss = loss.item()
 
-        if top5_flag:
-            return top1_error, top1_loss, top5_error
-        else:
-            return top1_error, top1_loss
+    if top5_flag:
+        return top1_error, top1_loss, top5_error
+    else:
+        return top1_error, top1_loss
 
+@torch.no_grad()
 def accuracy(output, target, topk=(1,)):
     """Computes the precision@k for the specified values of k"""
-    with torch.no_grad():
-        maxk = max(topk)
-        batch_size = target.size(0)
+    maxk = max(topk)
+    batch_size = target.size(0)
 
-        _, pred = output.topk(maxk, 1, True, True)
-        pred = pred.t()
-        correct = pred.eq(target.view(1, -1).expand_as(pred))
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
 
-        res = []
-        for k in topk:
-            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
-            res.append(correct_k.mul_(100.0 / batch_size).item())
-        return res
+    res = []
+    for k in topk:
+        correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+        res.append(correct_k.mul_(100.0 / batch_size).item())
+    return res
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
@@ -106,3 +120,223 @@ class AverageMeter(object):
         self.sum += val * n
         self.count += n
         self.avg = self.sum / self.count
+
+
+'''
+DETECTION
+'''
+@torch.no_grad()
+def validate_det(data,
+        weights=None,  # model.pt path(s)
+        batch_size=32,  # batch size
+        imgsz=640,  # inference size (pixels)
+        conf_thres=0.001,  # confidence threshold
+        iou_thres=0.6,  # NMS IoU threshold
+        task='val',  # train, val, test, speed or study
+        device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+        single_cls=False,  # treat as single-class dataset
+        augment=False,  # augmented inference
+        verbose=False,  # verbose output
+        save_txt=False,  # save results to *.txt
+        save_hybrid=False,  # save label+prediction hybrid results to *.txt
+        save_conf=False,  # save confidences in --save-txt labels
+        save_json=False,  # save a COCO-JSON results file
+        project=YOLOROOT / 'runs/val',  # save to project/name
+        name='exp',  # save to project/name
+        exist_ok=False,  # existing project/name ok, do not increment
+        half=True,  # use FP16 half-precision inference
+        dnn=False,  # use OpenCV DNN for ONNX inference
+        model=None,
+        dataloader=None,
+        save_dir=Path(''),
+        plots=True,
+        callbacks=Callbacks(),
+        compute_loss=None,
+        ):
+    
+    device = select_device(device, batch_size=batch_size)
+
+    # Directories
+    save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
+    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+
+    # Load model
+    model = DetectMultiBackend(weights, device=device, dnn=dnn)
+    stride, pt = model.stride, model.pt
+    imgsz = check_img_size(imgsz, s=stride)  # check image size
+    half &= pt and device.type != 'cpu'  # half precision only supported by PyTorch on CUDA
+    if pt:
+        model.model.half() if half else model.model.float()
+    else:
+        half = False
+        batch_size = 1  # export.py models default to batch-size 1
+        device = torch.device('cpu')
+        LOGGER.info(f'Forcing --batch-size 1 square inference shape(1,3,{imgsz},{imgsz}) for non-PyTorch backends')
+
+    # Data
+    data = check_dataset(data)  # check
+
+    # Configure
+    model.eval()
+    is_coco = isinstance(data.get('val'), str) and data['val'].endswith('coco/val2017.txt')  # COCO dataset
+    nc = 1 if single_cls else int(data['nc'])  # number of classes
+    iouv = torch.linspace(0.5, 0.95, 10).to(device)  # iou vector for mAP@0.5:0.95
+    niou = iouv.numel()
+
+    # Dataloader
+    if not training:
+        if pt and device.type != 'cpu':
+            model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.model.parameters())))  # warmup
+        pad = 0.0 if task == 'speed' else 0.5
+        task = task if task in ('train', 'val', 'test') else 'val'  # path to train/val/test images
+        dataloader = create_dataloader(data[task], imgsz, batch_size, stride, single_cls, pad=pad, rect=pt,
+                                       prefix=colorstr(f'{task}: '))[0]
+
+    seen = 0
+    confusion_matrix = ConfusionMatrix(nc=nc)
+    names = {k: v for k, v in enumerate(model.names if hasattr(model, 'names') else model.module.names)}
+    class_map = coco80_to_coco91_class() if is_coco else list(range(1000))
+    s = ('%20s' + '%11s' * 6) % ('Class', 'Images', 'Labels', 'P', 'R', 'mAP@.5', 'mAP@.5:.95')
+    dt, p, r, f1, mp, mr, map50, map = [0.0, 0.0, 0.0], 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+    loss = torch.zeros(3, device=device)
+    jdict, stats, ap, ap_class = [], [], [], []
+    pbar = tqdm(dataloader, desc=s, ncols=NCOLS, bar_format='{l_bar}{bar:10}{r_bar}{bar:-10b}')  # progress bar
+    for batch_i, (im, targets, paths, shapes) in enumerate(pbar):
+        t1 = time_sync()
+        if pt:
+            im = im.to(device, non_blocking=True)
+            targets = targets.to(device)
+        im = im.half() if half else im.float()  # uint8 to fp16/32
+        im /= 255  # 0 - 255 to 0.0 - 1.0
+        nb, _, height, width = im.shape  # batch size, channels, height, width
+        t2 = time_sync()
+        dt[0] += t2 - t1
+
+        # Inference
+        out, train_out = model(im) if training else model(im, augment=augment, val=True)  # inference, loss outputs
+        dt[1] += time_sync() - t2
+
+        # Loss
+        if compute_loss:
+            loss += compute_loss([x.float() for x in train_out], targets)[1]  # box, obj, cls
+
+        # NMS
+        targets[:, 2:] *= torch.Tensor([width, height, width, height]).to(device)  # to pixels
+        lb = [targets[targets[:, 0] == i, 1:] for i in range(nb)] if save_hybrid else []  # for autolabelling
+        t3 = time_sync()
+        out = non_max_suppression(out, conf_thres, iou_thres, labels=lb, multi_label=True, agnostic=single_cls)
+        dt[2] += time_sync() - t3
+
+        # Metrics
+        for si, pred in enumerate(out):
+            labels = targets[targets[:, 0] == si, 1:]
+            nl = len(labels)
+            tcls = labels[:, 0].tolist() if nl else []  # target class
+            path, shape = Path(paths[si]), shapes[si][0]
+            seen += 1
+
+            if len(pred) == 0:
+                if nl:
+                    stats.append((torch.zeros(0, niou, dtype=torch.bool), torch.Tensor(), torch.Tensor(), tcls))
+                continue
+
+            # Predictions
+            if single_cls:
+                pred[:, 5] = 0
+            predn = pred.clone()
+            scale_coords(im[si].shape[1:], predn[:, :4], shape, shapes[si][1])  # native-space pred
+
+            # Evaluate
+            if nl:
+                tbox = xywh2xyxy(labels[:, 1:5])  # target boxes
+                scale_coords(im[si].shape[1:], tbox, shape, shapes[si][1])  # native-space labels
+                labelsn = torch.cat((labels[:, 0:1], tbox), 1)  # native-space labels
+                correct = process_batch(predn, labelsn, iouv)
+                if plots:
+                    confusion_matrix.process_batch(predn, labelsn)
+            else:
+                correct = torch.zeros(pred.shape[0], niou, dtype=torch.bool)
+            stats.append((correct.cpu(), pred[:, 4].cpu(), pred[:, 5].cpu(), tcls))  # (correct, conf, pcls, tcls)
+
+            # Save/log
+            if save_txt:
+                save_one_txt(predn, save_conf, shape, file=save_dir / 'labels' / (path.stem + '.txt'))
+            if save_json:
+                save_one_json(predn, jdict, path, class_map)  # append to COCO-JSON dictionary
+            callbacks.run('on_val_image_end', pred, predn, path, names, im[si])
+
+        # Plot images
+        if plots and batch_i < 3:
+            f = save_dir / f'val_batch{batch_i}_labels.jpg'  # labels
+            Thread(target=plot_images, args=(im, targets, paths, f, names), daemon=True).start()
+            f = save_dir / f'val_batch{batch_i}_pred.jpg'  # predictions
+            Thread(target=plot_images, args=(im, output_to_target(out), paths, f, names), daemon=True).start()
+
+    # Compute metrics
+    stats = [np.concatenate(x, 0) for x in zip(*stats)]  # to numpy
+    if len(stats) and stats[0].any():
+        p, r, ap, f1, ap_class = ap_per_class(*stats, plot=plots, save_dir=save_dir, names=names)
+        ap50, ap = ap[:, 0], ap.mean(1)  # AP@0.5, AP@0.5:0.95
+        mp, mr, map50, map = p.mean(), r.mean(), ap50.mean(), ap.mean()
+        nt = np.bincount(stats[3].astype(np.int64), minlength=nc)  # number of targets per class
+    else:
+        nt = torch.zeros(1)
+
+    # Print results
+    pf = '%20s' + '%11i' * 2 + '%11.3g' * 4  # print format
+    LOGGER.info(pf % ('all', seen, nt.sum(), mp, mr, map50, map))
+
+    # Print results per class
+    if (verbose or (nc < 50 and not training)) and nc > 1 and len(stats):
+        for i, c in enumerate(ap_class):
+            LOGGER.info(pf % (names[c], seen, nt[c], p[i], r[i], ap50[i], ap[i]))
+
+    # Print speeds
+    t = tuple(x / seen * 1E3 for x in dt)  # speeds per image
+    if not training:
+        shape = (batch_size, 3, imgsz, imgsz)
+        LOGGER.info(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {shape}' % t)
+
+    # Plots
+    if plots:
+        confusion_matrix.plot(save_dir=save_dir, names=list(names.values()))
+        callbacks.run('on_val_end')
+
+    # Save JSON
+    if save_json and len(jdict):
+        w = Path(weights[0] if isinstance(weights, list) else weights).stem if weights is not None else ''  # weights
+        anno_json = str(Path(data.get('path', '../coco')) / 'annotations/instances_val2017.json')  # annotations json
+        pred_json = str(save_dir / f"{w}_predictions.json")  # predictions json
+        LOGGER.info(f'\nEvaluating pycocotools mAP... saving {pred_json}...')
+        with open(pred_json, 'w') as f:
+            json.dump(jdict, f)
+
+        try:  # https://github.com/cocodataset/cocoapi/blob/master/PythonAPI/pycocoEvalDemo.ipynb
+            check_requirements(['pycocotools'])
+            from pycocotools.coco import COCO
+            from pycocotools.cocoeval import COCOeval
+
+            anno = COCO(anno_json)  # init annotations api
+            pred = anno.loadRes(pred_json)  # init predictions api
+            eval = COCOeval(anno, pred, 'bbox')
+            if is_coco:
+                eval.params.imgIds = [int(Path(x).stem) for x in dataloader.dataset.img_files]  # image IDs to evaluate
+            eval.evaluate()
+            eval.accumulate()
+            eval.summarize()
+            map, map50 = eval.stats[:2]  # update results (mAP@0.5:0.95, mAP@0.5)
+        except Exception as e:
+            LOGGER.info(f'pycocotools unable to run: {e}')
+
+    # Return results
+    model.float()  # for training
+    if not training:
+        s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
+        LOGGER.info(f"Results saved to {colorstr('bold', save_dir)}{s}")
+    maps = np.zeros(nc) + map
+    for i, c in enumerate(ap_class):
+        maps[c] = ap[i]
+    return (mp, mr, map50, map, *(loss.cpu() / len(dataloader)).tolist()), maps, t
+
+if __name__ == "__main__":
+    pass
